79722ff19652287c5af57e4fd44509a7fb2b7967
CLAUDIO CORDERO FERNANDEZ <claudio.cordero@ulatina.net>
2017-11-27 21:24:31 -0600

    Category set up

---
+++ ProyectoDiginamo/Areas/Admin/Controllers/ShopController.cs
@@ -0,0 +1,34 @@
+﻿using ProyectoDiginamo.Models.Data;
+using ProyectoDiginamo.Models.ViewModels.Shop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoDiginamo.Areas.Admin.Controllers
+{
+    public class ShopController : Controller
+    {
+        // GET: Admin/Shop/Categories
+        public ActionResult Categories()
+        {
+
+            //Declare list of models
+            List<CategoryVM> categoryVmList;
+
+            using (Db db = new Db())
+            {
+                // Init the list
+                categoryVmList = db.Categories
+                    .ToArray()
+                    .OrderBy(x => x.Sorting)
+                    .Select(x => new CategoryVM(x))
+                    .ToList();
+            }
+            // Return view with list
+
+            return View(categoryVmList);
+        }
+    }
+}
--- ProyectoDiginamo/Models/Data/db.cs
+++ ProyectoDiginamo/Models/Data/db.cs
@@ -11,6 +11,7 @@ namespace ProyectoDiginamo.Models.Data
 
         public DbSet<PageDTO> Pages { get; set; }
         public DbSet<SidebarDTO> Sidebar { get; set; }
+        public DbSet<CategoryDTO> Categories { get; set; }
 
     }
 
